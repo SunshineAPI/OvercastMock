357c8fad6dbca3a7b13beae20e5fb83a6474c52e
Jake <jake0oo0dev@gmail.com>
2015-07-24 23:13:23 -0500

    Update scraped pages 2015_07_24

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+if ! git remote | grep mock > /dev/null; then
+  git remote add mock git@github.com:SunshineAPI/sunshine-mock.github.io.git  
+fi
+
+git add scraped/
+today=`date '+%Y_%m_%d'`;
+git commit -am "Update scraped pages ${today}"
+git subtree push --prefix scraped mock gh-pages
